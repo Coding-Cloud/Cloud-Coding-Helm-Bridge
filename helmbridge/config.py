624c79eb33d33e9a07eff406fa177531e9777f7e
pooetitu <pooetitu@gmail.com>
2022-03-27 17:18:19 +0200

    Change react template

--- helmbridge/config.py
+++ helmbridge/config.py
@@ -1,4 +1,4 @@
 language_config = {'ANGULAR': {'values': '/angular-runner/values.yaml',
                                'template': 'https://github.com/tomastrajan/angular-ngrx-material-starter.git'},
                    'REACT': {'values': '/react-runner/values.yaml',
-                             'template': 'https://github.com/react-boilerplate/react-boilerplate.git'}}
+                             'template': 'https://github.com/cruip/open-react-template'}}
