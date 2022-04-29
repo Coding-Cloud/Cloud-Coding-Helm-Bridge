81e9f40a3ebea4a0971d3a60412d90435f40bf9f
pooetitu <pooetitu@gmail.com>
2022-04-29 23:21:17 +0200

    [CC-95]: Change angular template

--- helmbridge/config.py
+++ helmbridge/config.py
@@ -1,4 +1,4 @@
 language_config = {'ANGULAR': {'values': '/angular-values.yaml',
-                               'template': 'https://github.com/tomastrajan/angular-ngrx-material-starter.git'},
+                               'template': 'https://github.com/Coding-Cloud/angular-template.git'},
                    'REACT': {'values': '/react-values.yaml',
                              'template': 'https://github.com/soumyajit4419/Portfolio.git'}}
