7a438c444417937aa273004c0eaa9234d99ab3ba
pooetitu <pooetitu@gmail.com>
2022-04-30 12:40:20 +0200

    [CC-95]: Add logger to create project thread

--- helmbridge/project.py
+++ helmbridge/project.py
@@ -2,19 +2,25 @@ import os
 import shutil
 
 import requests
+from flask import Flask
 from git import Repo
 
 from helmbridge.config import language_config
 
+logger = Flask(__name__).logger
+
 
 def create_project(project_id, language):
-    repo_path = os.path.join(os.environ['REPOSITORIES_PATH'], project_id)
-    Repo.clone_from(language_config[language]['template'], repo_path)
-    shutil.rmtree(os.path.join(repo_path, '.git'))
-    repo = Repo.init(repo_path)
-    repo.git.add(all=True)
-    repo.index.commit('1 - Initial version')
-    requests.patch(os.environ['API_URL'] + '/projects/' + project_id + '/initialised')
+    try:
+        repo_path = os.path.join(os.environ['REPOSITORIES_PATH'], project_id)
+        Repo.clone_from(language_config[language]['template'], repo_path)
+        shutil.rmtree(os.path.join(repo_path, '.git'))
+        repo = Repo.init(repo_path)
+        repo.git.add(all=True)
+        repo.index.commit('1 - Initial version')
+        requests.patch(os.environ['API_URL'] + '/projects/' + project_id + '/initialised')
+    except Exception as e:
+        logger.error(e)
 
 
 def delete_project(project_id):
