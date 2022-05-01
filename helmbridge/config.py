839eff26dc6bc8a541021a66fbd755d6f5b9a409
pooetitu <pooetitu@gmail.com>
2022-05-01 20:40:30 +0200

    [CC-110]: Add quarkus config

--- helmbridge/config.py
+++ helmbridge/config.py
@@ -1,4 +1,6 @@
 language_config = {'ANGULAR': {'values': 'angular-values.yaml',
                                'template': 'https://github.com/Coding-Cloud/angular-template.git'},
                    'REACT': {'values': 'react-values.yaml',
-                             'template': 'https://github.com/soumyajit4419/Portfolio.git'}}
+                             'template': 'https://github.com/soumyajit4419/Portfolio.git'},
+                   'QUARKUS': {'values': 'quarkus-values.yaml',
+                               'template': 'https://github.com/Coding-Cloud/quarkus-template.git'}}
