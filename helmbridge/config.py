07d5fd59a0a2fee894297a346b55ac4c74e362db
pooetitu <pooetitu@gmail.com>
2022-07-27 18:09:23 +0200

    Add cloud coding runner values

--- helmbridge/config.py
+++ helmbridge/config.py
@@ -7,4 +7,7 @@ language_config = {'ANGULAR': {'values': 'angular-values.yaml',
                    'NESTJS': {'values': 'nest-values.yaml',
                               'template': 'https://github.com/Coding-Cloud/nest-template.git'},
                    'FLASK': {'values': 'flask-values.yaml',
-                             'template': 'https://github.com/Coding-Cloud/flask-template.git'}}
+                             'template': 'https://github.com/Coding-Cloud/flask-template.git'},
+                   'CC': {'values': 'cloud-coding-values.yaml',
+                             'template': 'https://github.com/Coding-Cloud/Cloud-Coding-Angular-App.git'}
+                   }
